ddfe72db1d19728742bec25fcbf2253642189396
jooany <jjj423888@gmail.com>
2023-05-13 15:07:13 +0900

    first commit

---
+++ src/main/java/com/jooany/letsdeal/LetsdealApplication.java
@@ -0,0 +1,13 @@
+package com.jooany.letsdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsdealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jooany/letsdeal/LetsdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jooany.letsdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsdealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
