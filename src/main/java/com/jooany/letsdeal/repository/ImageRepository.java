0b149c10ed160cffe95d9b1d7b222ca2e5b9bb36
jooany <jjj423888@gmail.com>
2023-07-27 02:30:46 +0900

    Spring Data JPA Repository 관련 설명 주석 추가

--- src/main/java/com/jooany/letsdeal/repository/ImageRepository.java
+++ src/main/java/com/jooany/letsdeal/repository/ImageRepository.java
@@ -9,14 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 /*
-* @Modifying : 데이터베이스의 데이터 수정 작업을 실행할 때 필요한 어노테이션
+* @Modifying : 데이터베이스의 데이터 변경 작업이 있을 때 필요한 어노테이션
 * @Query : 사용자 쿼리 정의
 * @Param : 쿼리에 사용할 파라미터를 지정
 * */
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long>, ImageCustomRepository{
 
-    // 기본적으로 제공하는 deleteBySale 을 사용하게 되면, select문 1개, delete문이 N개 즉 N+1 문제가 발생한다.
+    // Method Name Query를 사용하여 삭제하면 select문 1개, delete문이 N개 즉 N+1 문제가 나타날 수 있다.
     // 이를 방지하기 위해, 하나의 delete문을 실행할 수 있는 쿼리를 직접 정의하였다.
     @Transactional
     @Modifying
--- src/main/java/com/jooany/letsdeal/repository/SaleRepository.java
+++ src/main/java/com/jooany/letsdeal/repository/SaleRepository.java
@@ -8,7 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 /*
+    Spring Data JPA 에서 메소드를 찾는 순서
+    1. 기본 제공하는 메소드
+    2. 메소드 이름에서 특정 규칙을 따라 생성하는 쿼리인 Method Name Query
+    3. @Query 어노테이션으로 사용자 정의된 쿼리 메소드
 
+    따라서, 기본으로 제공되는 deleteById 메소드는 @Query로 인하여 재정의된다.
  */
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long>, SaleCustomRepository{
@@ -16,5 +21,4 @@ public interface SaleRepository extends JpaRepository<Sale, Long>, SaleCustomRep
     @Modifying
     @Query("DELETE FROM Sale entity WHERE entity.id = :saleId")
     void deleteById(@Param("saleId") Long saleId);
-
 }
