fab23d9230319f1be20769deb97090b8470aa243
jooany <jjj423888@gmail.com>
2023-10-10 01:00:01 +0900

    테스트 DtoFixture에 요청DTO 생성 추가

--- src/main/java/com/jooany/letsdeal/controller/dto/request/MessageSendReq.java
+++ src/main/java/com/jooany/letsdeal/controller/dto/request/MessageSendReq.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class MessageSendReq {
     @NotNull(message = "NULL일 수 없습니다.")
