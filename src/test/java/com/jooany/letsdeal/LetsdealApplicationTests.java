681df020662afdaae48cc5dc91f4ecd174eac622
jooany <jjj423888@gmail.com>
2023-10-11 02:32:27 +0900

    test

--- src/test/java/com/jooany/letsdeal/LetsdealApplicationTests.java
+++ src/test/java/com/jooany/letsdeal/LetsdealApplicationTests.java
@@ -1,10 +1,8 @@
 package com.jooany.letsdeal;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class LetsdealApplicationTests {
 
 //	@Test
